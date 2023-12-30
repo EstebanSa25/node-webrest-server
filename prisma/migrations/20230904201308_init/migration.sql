b29471348b044d070c978597e54ad85d7d76ea04
EstebanSa25 <estebansalas2502@gmail.com>
2023-12-29 18:34:30 -0600

    Final seccion 15

--- prisma/migrations/20230904201308_init/migration.sql
+++ prisma/migrations/20230904201308_init/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "todo" (
     "id" SERIAL NOT NULL,
-    "text" VARCHAR(255) NOT NULL,
+    "text" VARCHAR NOT NULL,
     "completedAt" TIMESTAMP,
 
     CONSTRAINT "todo_pkey" PRIMARY KEY ("id")
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,22 @@
-import { envs } from './config/env';
+import { envs } from './config/envs';
 import { AppRoutes } from './presentation/routes';
 import { Server } from './presentation/server';
 
-(() => {
-    main();
+
+
+
+(async()=> {
+  main();
 })();
 
+
 function main() {
-    const server = new Server({
-        port: envs.PORT,
-        public_path: envs.PUBLIC_PATH,
-        routes: AppRoutes.routes,
-    });
-    server.start();
+
+  const server = new Server({
+    port: envs.PORT,
+    public_path: envs.PUBLIC_PATH,
+    routes: AppRoutes.routes,
+  });
+
+  server.start();
 }
